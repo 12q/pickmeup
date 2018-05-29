7f9c4ab0e69e104d8450b85c76e57626488d148f
Slava Plis <observleer@gmail.com>
2018-05-29 14:24:59 +0300

    Global: save tmp state.

--- PickMeUp/BaseViewController.swift
+++ PickMeUp/BaseViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class BaseViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindBaseSB(_ sender: Any) {
+        self.dismiss(animated: true) {}
+    }
+    
+    
     
 
     /*
