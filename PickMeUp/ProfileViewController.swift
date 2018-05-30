a8bcc1f607abce790bc16c4225cb6d41737f8832
Slava Plis <observleer@gmail.com>
2018-05-30 18:34:50 +0300

    ProfileViewController: connecting FB button.

--- PickMeUp/ProfileViewController.swift
+++ PickMeUp/ProfileViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var goToMapButton: UIButton!
+    
+    struct MyProfileRequest: GraphRequestProtocol {
+        struct Response: GraphResponseProtocol {
+            init(rawResponse: Any?) {
+                // Decode JSON from rawResponse into other properties here.
+            }
+        }
+        
+        var graphPath = "/me"
+        var parameters: [String : Any]? = ["fields": "id, name, picture"]
+        var accessToken = AccessToken.current
+        var httpMethod: GraphRequestHTTPMethod = .GET
+        var apiVersion: GraphAPIVersion = .defaultVersion
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        if let accessToken = AccessToken.current {
+            print(accessToken)
+        }
+        
+        let connection = GraphRequestConnection()
+        connection.add(MyProfileRequest()) { response, result in
+            switch result {
+            case .success(let response):
+                print("Custom Graph Request Succeeded: \(response)")
+//                print("My facebook id is \(response.dictionaryValue?["id"])")
+//                print("My name is \(response.dictionaryValue?["name"])")
+            case .failed(let error):
+                print("Custom Graph Request Failed: \(error)")
+            }
+        }
+        connection.start()
     }
 
     override func didReceiveMemoryWarning() {
