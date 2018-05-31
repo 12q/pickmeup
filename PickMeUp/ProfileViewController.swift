7e7fbc86d2daf0d268e0d854f84b31d1d8b7d766
Slava Plis <observleer@gmail.com>
2018-05-31 17:34:46 +0300

    ProfileViewController: connected FB GraphRequestConnection.

--- PickMeUp/ProfileViewController.swift
+++ PickMeUp/ProfileViewController.swift
@@ -9,63 +9,108 @@
 import UIKit
 import FacebookLogin
 import FacebookCore
+import Foundation
 
 class ProfileViewController: UIViewController {
     @IBOutlet weak var goToMapButton: UIButton!
+    @IBOutlet weak var userAva: UIImageView!
+    @IBOutlet weak var userName: UILabel!
     
     struct MyProfileRequest: GraphRequestProtocol {
-        struct Response: GraphResponseProtocol {
-            init(rawResponse: Any?) {
-                // Decode JSON from rawResponse into other properties here.
-            }
-        }
-        
         var graphPath = "/me"
         var parameters: [String : Any]? = ["fields": "id, name, picture"]
         var accessToken = AccessToken.current
         var httpMethod: GraphRequestHTTPMethod = .GET
         var apiVersion: GraphAPIVersion = .defaultVersion
+        
+        struct Response: GraphResponseProtocol {
+            public struct PublicProfile: Decodable {
+                public var id: String
+                public var name: String
+                public var picture: PublicImageData
+
+                enum CodingKeys: String, CodingKey {
+                    case id
+                    case name
+                    case picture
+                }
+                
+                public struct PublicImageData: Decodable {
+                    public var data: PublicImage
+                    enum CodingKeys: String, CodingKey {
+                        case data
+                    }
+                }
+                
+                public struct PublicImage: Decodable {
+                    public var width: Int
+                    public var height: Int
+                    public var url: URL
+                    
+                    enum CodingKeys: String, CodingKey {
+                        case width
+                        case height
+                        case url
+                    }
+                }
+            }
+            
+            var pub: PublicProfile?
+            
+            init(rawResponse: Any?) {
+                if let jsonData = try? JSONSerialization.data(withJSONObject: rawResponse, options: []) {
+                    let decoder = JSONDecoder()
+                    do {
+                        let model = try decoder.decode(PublicProfile.self, from: jsonData)
+                        pub = model
+                        print("model: \(model)")
+                    } catch let parsingError {
+                        print("Error", parsingError)
+                    }
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
-        view.addSubview(loginButton)
-        
-        if let accessToken = AccessToken.current {
-            print(accessToken)
-        }
-        
+        setFbButton()
+        launchFacebookConnection()
+    }
+    
+    // MARK: - FB connect
+    
+    private func launchFacebookConnection() {
         let connection = GraphRequestConnection()
-        connection.add(MyProfileRequest()) { response, result in
+        connection.add(MyProfileRequest()) { (response, result) in
             switch result {
             case .success(let response):
-                print("Custom Graph Request Succeeded: \(response)")
-//                print("My facebook id is \(response.dictionaryValue?["id"])")
-//                print("My name is \(response.dictionaryValue?["name"])")
+                print("response.pub:")
+                self.userName.text = response.pub?.name
+                if let url = response.pub?.picture.data.url {
+                    if let data = try? Data(contentsOf: url) {
+                        self.userAva.image = UIImage(data: data)
+                    }
+                }
+
             case .failed(let error):
                 print("Custom Graph Request Failed: \(error)")
             }
         }
         connection.start()
     }
+    
+    // MARK: - UI
+    
+    private func setFbButton() {
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+    }
+
+    // MARK: - Sys - Memory Warning
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
