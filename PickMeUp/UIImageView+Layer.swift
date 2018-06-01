b3102407b66a114f5948ac232c828259c752eff6
Slava Plis <observleer@gmail.com>
2018-06-01 19:43:48 +0300

    Extension: added IBDesignable props for the UIImageView.

---
+++ PickMeUp/UIImageView+Layer.swift
@@ -0,0 +1,69 @@
+//
+//  UIImageView+Layer.swift
+//  PickMeUp
+//
+//  Created by Slava Plis on 6/1/18.
+//  Copyright © 2018 Slava Plis. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+extension UIImageView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+}
