d4ac3db354f000e806e373e7cc64509e75f7fa74
Slava Plis <observleer@gmail.com>
2018-06-01 13:40:42 +0300

    ProfileViewController: removed dump functionality.

--- PickMeUp/ProfileViewController.swift
+++ PickMeUp/ProfileViewController.swift
@@ -7,106 +7,21 @@
 //
 
 import UIKit
-import FacebookLogin
-import FacebookCore
 import Foundation
+import FacebookCore
+import FacebookLogin
 
 class ProfileViewController: UIViewController {
     @IBOutlet weak var goToMapButton: UIButton!
     @IBOutlet weak var userAva: UIImageView!
     @IBOutlet weak var userName: UILabel!
     
-    struct MyProfileRequest: GraphRequestProtocol {
-        var graphPath = "/me"
-        var parameters: [String : Any]? = ["fields": "id, name, picture"]
-        var accessToken = AccessToken.current
-        var httpMethod: GraphRequestHTTPMethod = .GET
-        var apiVersion: GraphAPIVersion = .defaultVersion
-        
-        struct Response: GraphResponseProtocol {
-            public struct PublicProfile: Decodable {
-                public var id: String
-                public var name: String
-                public var picture: PublicImageData
-
-                enum CodingKeys: String, CodingKey {
-                    case id
-                    case name
-                    case picture
-                }
-                
-                public struct PublicImageData: Decodable {
-                    public var data: PublicImage
-                    enum CodingKeys: String, CodingKey {
-                        case data
-                    }
-                }
-                
-                public struct PublicImage: Decodable {
-                    public var width: Int
-                    public var height: Int
-                    public var url: URL
-                    
-                    enum CodingKeys: String, CodingKey {
-                        case width
-                        case height
-                        case url
-                    }
-                }
-            }
-            
-            var pub: PublicProfile?
-            
-            init(rawResponse: Any?) {
-                if let jsonData = try? JSONSerialization.data(withJSONObject: rawResponse, options: []) {
-                    let decoder = JSONDecoder()
-                    do {
-                        let model = try decoder.decode(PublicProfile.self, from: jsonData)
-                        pub = model
-                        print("model: \(model)")
-                    } catch let parsingError {
-                        print("Error", parsingError)
-                    }
-                }
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setFbButton()
-        launchFacebookConnection()
     }
     
-    // MARK: - FB connect
-    
-    private func launchFacebookConnection() {
-        let connection = GraphRequestConnection()
-        connection.add(MyProfileRequest()) { (response, result) in
-            switch result {
-            case .success(let response):
-                print("response.pub:")
-                self.userName.text = response.pub?.name
-                if let url = response.pub?.picture.data.url {
-                    if let data = try? Data(contentsOf: url) {
-                        self.userAva.image = UIImage(data: data)
-                    }
-                }
-
-            case .failed(let error):
-                print("Custom Graph Request Failed: \(error)")
-            }
-        }
-        connection.start()
-    }
     
     // MARK: - UI
-    
-    private func setFbButton() {
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
-        view.addSubview(loginButton)
-    }
 
     // MARK: - Sys - Memory Warning
 
